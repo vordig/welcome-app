9961804c7e35c3ba189ea8fc70b456a50c87b911
Andrey Seregin <vordig@gmail.com>
2024-12-11 20:13:21 +0300

    First lesson

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {ListContainerComponent} from './list-container/list-container.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [
+    RouterOutlet,
+    ListContainerComponent
+  ],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'welcome-app';
